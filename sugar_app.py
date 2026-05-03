f67ecc55a91c24dfc0b09e5b985a4bed06cd2214
squatic <squatic@gmail.com>
2026-05-04 01:21:17 +0800

    Fix HTML color code in sugar_app.py

--- sugar_app.py
+++ sugar_app.py
@@ -746,7 +746,7 @@ with st.sidebar:
     st.markdown("""
     <div style="font-family:'Playfair Display',serif; font-size:1.3rem; font-weight:700;
          color:#ccfa34; margin-bottom:0.2rem; line-height:1.2;">
-      🍬 Sugar Price<br><span style="font-size:0.9rem;color:##ffffff;font-family:'Space Mono',monospace;
+      🍬 Sugar Price<br><span style="font-size:0.9rem;color:#ffffff;font-family:'Space Mono',monospace;
       font-style:normal;font-weight:400;letter-spacing:0.05em;">Montecarlo Risk Model | Price Prediction</span>
     </div>
     """, unsafe_allow_html=True)
